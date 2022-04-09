e048d5d402f3a2b55c02aaf0534a083240b400ef
kuanweilien <lksh9921120@gmail.com>
2022-04-09 22:20:10 +0800

    add auth

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -9,17 +10,12 @@ function App() {
         <p>
           Please Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth !</h1>
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
